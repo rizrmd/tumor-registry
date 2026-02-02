46512ca34ee247574aec9e0918373c24c589b9f1
rizky <rizky05@gmail.com>
2026-02-02 17:18:39 +0700

    refactor: rename executable from "desktop" to "INAMSOS"
    - Update wails.json: name and outputfilename to INAMSOS
    - Update main.go: window title to INAMSOS

--- desktop/main.go
+++ desktop/main.go
@@ -17,9 +17,9 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "desktop",
+		Title:  "INAMSOS",
 		Width:  1024,
-		Height: 768,
+		Height:  768,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
